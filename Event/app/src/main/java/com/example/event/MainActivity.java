75adf9902dc5c438d9c47e2261fd5f4d2131bce3
ardantyo80 <77425745+ardantyo80@users.noreply.github.com>
2021-12-02 00:33:31 +0700

    Add files via upload

---
+++ Event/app/src/main/java/com/example/event/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import and1.2.1
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
